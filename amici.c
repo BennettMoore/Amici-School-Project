856f5a79aa480aa3b6220fc13d1b0b5001422381
Bennett W Moore <bwm7637@cs.rit.edu>
2021-09-30 12:18:36 -0400

    Created basic amici.c file with no functionality.

---
+++ amici.c
@@ -0,0 +1,24 @@
+/*
+ * file: amici.c
+ *
+ * A new social network that can add and remove users, and manage friendships between users
+ *
+ * @author Bennett Moore bwm7637@rit.edu
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "hash.h"
+#include "table.h"
+
+/*
+ * The main method
+ *
+ * @param argc The number of arguments
+ * @param argv An array of string arguments
+ */
+int main(int argc, char * argv[]){
+	
+	return 0;
+}
