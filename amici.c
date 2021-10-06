4b094daebc97d7ebcbdf16c6fd2fbff7c8a35425
Bennett W Moore <bwm7637@cs.rit.edu>
2021-10-05 20:11:30 -0400

    Users can now be added to the table, and all add functionality is working

--- amici.c
+++ amici.c
@@ -6,6 +6,8 @@
  * @author Bennett Moore bwm7637@rit.edu
  */
 
+#define _POSIX_C_SOURCE 200809L //Allows strdup to work
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,9 +16,8 @@
 #include "hash.h"
 #include "table.h"
 
-#define _POSIX_C_SOURCE 200809L //Allows for strdup to work
 #define BUF_SIZE 1024
-#define MAX_COMMANDS 3
+#define MAX_COMMANDS 4
 #define FRIEND_BLOCK 10
 
 typedef struct person_s{
@@ -28,7 +29,10 @@ typedef struct person_s{
 	size_t max_friends;		// Maximum number of friends
 } person_t;
 
+//Global Variables
 Table t;
+int friends;
+int people;
 
 
 /*
@@ -36,6 +40,7 @@ Table t;
  *
  * @param key The key of the table entry
  * @param value The value of the table entry
+ * @pre value is of type person_t
  *
  */
 void tablePrint(const void* key, const void* value){
@@ -49,7 +54,7 @@ void tablePrint(const void* key, const void* value){
  *
  * @param key The key of the table entry
  * @param value The value of the table entry
- *
+ * @pre value is of type person_t
  */
 void tableDel(void* key, void* value){
 	(void)key;
@@ -62,10 +67,13 @@ void tableDel(void* key, void* value){
 		free(p1->friends[i]);
 	}
 	free(p1->friends);
+	free(p1);
 }
 
 /*
  * Clears database and reinitializes storage
+ * 
+ * @param is_quit whether the user is quitting or not
  *
  */
 void reformat(bool is_quit){
@@ -90,6 +98,7 @@ void printStats(){
  * Prints info about a user
  *
  * @param handle The handle of a user
+ * @pre handle is a valid user
  *
  */
 void printInfo(char * handle){
@@ -102,7 +111,8 @@ void printInfo(char * handle){
 /*
  * Adds or removes two users to/from each others' friend networks
  *
- * @param handles The handles of the two users
+ * @param handles[] The handles of the two users
+ * @pre handles[] has two entries
  *
  */
 void friend(char * handles[], bool is_friendly){
@@ -118,42 +128,66 @@ void friend(char * handles[], bool is_friendly){
  * Determines what command the user is attempting to perform, and run the function prescribed to it
  *
  * @param data An array of string commands
+ * @pre Commands are separated into tokens
  *
  */
-void parseCommands(char * data[MAX_COMMANDS]){
-	if(strcmp(data[0], "add") == 0){
-		//Format: add first_name last_name handle
-		//If valid and vacant, add new person to hash table	
+void parseCommands(char ** data){
+	if(strcmp(data[0], "add") == 0){			//Add new user
+		//Validate command
+		if((data[1] != NULL && data[2] != NULL && data[3] != NULL) && (strlen(data[1]) > 0 && strlen(data[2]) > 0 && strlen(data[3]) > 0)){
+			if(!ht_has(t, (void*)data[3])){ 
+				//Person is initialized
+				person_t* new_person = calloc(1, sizeof(person_t));
+				new_person->first_name = strdup(data[1]);
+				new_person->last_name = strdup(data[2]);
+				new_person->handle = strdup(data[3]);
+				new_person->friends = (person_t**)calloc(FRIEND_BLOCK, sizeof(person_t*));
+				new_person->friend_count = 0;
+				new_person->max_friends = FRIEND_BLOCK;
+				
+				//Put new person in table and add to people count
+				ht_put(t, (void*)new_person->handle, (void*)new_person);
+				people++;
+			}
+			else{ //Handle is taken
+				fprintf(stderr, "error: The handle '%s' is taken by another user\n", data[1]);
+			}
+		}
+		else{ //Invalid Command structure
+			fprintf(stderr, "error: add command usage: first-name last-name handle\n");
+		}
 	}
-	else if(strcmp(data[0], "friend") == 0){
+	else if(strcmp(data[0], "friend") == 0){		//Make two users friends
 		//Format: friend handle1 handle2
 		//If valid, send to friend function (specify friend)
 	}
-	else if(strcmp(data[0], "init") == 0){
+	else if(strcmp(data[0], "init") == 0){			//Clear table and reset it
 		//Format: init
 		//Send to reformat function (specify not quitting)
 	}
-	else if(strcmp(data[0], "print") == 0){
+	else if(strcmp(data[0], "print") == 0){			//Print data on a specific user
 		//Format: print handle
 		//If valid, send to printInfo function
 	}
-	else if(strcmp(data[0], "quit") == 0){
+	else if(strcmp(data[0], "quit") == 0){			//Clear table and exit program
 		//Format: quit
 		//Send to reformat function, then exit program (specify quitting)
 	}
-	else if(strcmp(data[0], "size") == 0){
+	else if(strcmp(data[0], "size") == 0){			//Print number of friends a user has
 		//Format: size handle
 		//If valid, print friend_count of user
 	}
-	else if(strcmp(data[0], "stats") == 0){
+	else if(strcmp(data[0], "stats") == 0){			//Print cumulative user data
 		//Format: stats
 		//Send to stat function
 	}
-	else if(strcmp(data[0], "unfriend") == 0){
+	else if(strcmp(data[0], "unfriend") == 0){		//Make two users not friends
 		//Format: unfriend handle1 handle2
 		//If valid, send to friend function (specify unfriend)
 	}
-		//Else do nothing
+	else{ 							//Catch any unrecognizable commands
+		fputs("error: not a valid command", stderr);
+	}
 }
 
 /*
@@ -172,8 +206,8 @@ int main(void){
 	while(true){
 		printf("amici> ");
 		fgets(buffer, BUF_SIZE, stdin);
+		test = strtok(buffer, delim);
 		for(int i = 0; i < MAX_COMMANDS; i++){
-			test = strtok(buffer, delim);
 			if(test != NULL){
 				input[i] = (char *)realloc(input[i], strlen(test)+1);
 				strcpy(input[i], test);
